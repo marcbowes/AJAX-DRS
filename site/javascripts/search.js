320d7f49097a5222be197f9d64bfb623aa795728
Marc Bowes <marcbowes@gmail.com>
2009-08-10 19:07:10 +0200

    made search work with soft-loading of indices

--- site/javascripts/search.js
+++ site/javascripts/search.js
@@ -1,33 +1,57 @@
+var indices = new Array();
+var untappedIndices = 0;
+
 /* On DOM-Ready */
 $(function() {
-  /* Unobtrusively apply behavior to the page */
-  
-  /* #1 ]]    Clicking search */
+  /* Clicking search */
   $("#search_submit").live("click", function() {
-    results(find($("#search_terms").val()));
+    find($("#search_terms").val());
     return false; /* disable redirection */
   });
 });
 
 function find(string) {
   var terms = string.split(/\s/);
+  untappedIndices = terms.length;
   
   /* for some reason a foreach prints integers? */
   for (i = 0; i < terms.length; i++) {
-    var term = terms[i];
-    var index = loadIndex(term);
-    /* HAX - pretend its loaded ;p */
-    index = "file1";
-    return index;
+    tapIndex(terms[i], string);
   }
 }
 
-function loadIndex(word) {
-  $.get("indices/" + word, null, function(index) {
-    return index.split("\n");
-  });
+function find_real(string) {
+  var terms = string.split(/\s/);
+  
+  /* for some reason a foreach prints integers? */
+  for (i = 0; i < terms.length; i++) {
+    alert("term: " + terms[i] + " -- \n" + indices[terms[i]]);
+  }
 }
 
-function results(list) {
-  $("#search_results").html(list);
+function tapIndex(word, searchString) {
+  if (!indices[word]) {
+    try {
+      $.get("indices/" + word, null, function(index) {
+        var listings = index.split("\n");
+        indices[word] = listings;
+        tapComplete(word, searchString);
+      });
+    } catch (error) {
+      tapComplete(word, searchString);
+    }
+  } else {
+    tapComplete(word, searchString);
+  }
+}
+
+function tapComplete(word, searchString) {
+  untappedIndices--;
+  if (untappedIndices == 0) {
+    find_real(searchString);
+  }
+}
+
+function results(results) {
+  $("#search_results").html(results);
 }
