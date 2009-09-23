6e47721a40730136d6f0f93936e23cc4a0021e71
Marc Bowes <marc@saidin.(none)>
2009-09-23 11:49:45 +0200

    properly saving metadata indices

--- preprocessor/src/Indexer.java
+++ preprocessor/src/Indexer.java
@@ -90,22 +90,8 @@ public class Indexer {
             s.stem();
             word = s.toString();
             
-            HashMap<String, Integer> documentToCount;
-            if(index.get("__root__").containsKey(word)) {
-              documentToCount = index.get("__root__").get(word);
-            } else {
-              documentToCount = new HashMap<String, Integer>();
-              index.get("__root__").put(word, documentToCount);
-            }
-            
-            Integer count;
-            if(documentToCount.containsKey(filename)) {
-              count = documentToCount.get(filename);
-            } else {
-              count = Integer.valueOf(0);
-            }
-            count = Integer.valueOf(count.intValue() + 1);
-            documentToCount.put(filename, count);
+            indexWord("__root__", word, filename);
+            indexWord(metafield, word, filename);
           } /* for each word */
         }
       }
@@ -118,12 +104,39 @@ public class Indexer {
     }
   }
   
+  public void indexWord(String metafield, String word, String filename) {
+    HashMap<String, Integer> documentToCount;
+    if(index.get(metafield).containsKey(word)) {
+      documentToCount = index.get(metafield).get(word);
+    } else {
+      documentToCount = new HashMap<String, Integer>();
+      index.get(metafield).put(word, documentToCount);
+    }
+    
+    Integer count;
+    if(documentToCount.containsKey(filename)) {
+      count = documentToCount.get(filename);
+    } else {
+      count = Integer.valueOf(0);
+    }
+    count = Integer.valueOf(count.intValue() + 1);
+    documentToCount.put(filename, count);
+  }
+  
   private void saveIndex() {
+    for (String metafield : index.keySet()) {
+      saveIndex(metafield);
+    }
+  }
+  
+  private void saveIndex(String metafield) {
     /* save to a file per word */
-    for (String word : index.get("__root__").keySet()) {
+    for (String word : index.get(metafield).keySet()) {
       try {
-        BufferedWriter file = new BufferedWriter(new FileWriter("../site/indices/" + word));
-        HashMap<String, Integer> documentToCount = index.get("__root__").get(word);
+        File _file = new File("../site/indices/" + (metafield == "__root__" ? "" : metafield));
+        _file.mkdirs();
+        BufferedWriter file = new BufferedWriter(new FileWriter(_file + "/" + word));
+        HashMap<String, Integer> documentToCount = index.get(metafield).get(word);
         for (String filename : documentToCount.keySet()) {
           file.write(filename + ":" + documentToCount.get(filename) + "\n");
         } /* for each file in which this word appears */
