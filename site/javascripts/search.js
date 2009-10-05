fcb3fc7a6dea3652e1daa3f29a7e2020a80196cc
Marc Bowes <marcbowes@gmail.com>
2009-10-05 12:54:29 +0200

    preperation for meta searching

--- site/javascripts/search.js
+++ site/javascripts/search.js
@@ -13,7 +13,11 @@ $(function() {
 function get_terms(string) {
   var terms = string.split(/[\s-\/]/);
   for (i = 0; i < terms.length; i++) {
-    terms[i] = stemmer(terms[i].replace(/[^\d\w]/g, ""));
+    var term = terms[i].replace(/[^\d\w]/g, "");
+    var idx = term.indexOf(":");
+    terms[i] = new Array(2);
+    terms[i][0] = (idx == -1 ? "" : term.substring(0, idx));
+    terms[i][1] = stemmer(idx == -1 ? term : term.substring(idx + 1));
   }
   return terms;
 }
@@ -25,7 +29,7 @@ function find(string) {
   untappedIndices = terms.length;
   
   for (i = 0; i < terms.length; i++) {
-    tapIndex(terms[i], string);
+    tapIndex(terms[i][0], terms[i][1], string);
   }
 }
 
@@ -35,7 +39,7 @@ function find_real(string) {
   
   /* for some reason a foreach prints integers? */
   for (i = 0; i < terms.length; i++) {
-    var index = indices[terms[i]];
+    var index = indices[terms[i][1]];
     for (item in index) {
       if (!items[item]) {
         items[item] = index[item];
@@ -55,7 +59,7 @@ function find_real(string) {
   results(result);
 }
 
-function tapIndex(word, searchString) {
+function tapIndex(meta, word, searchString) {
   if (!indices[word]) {
     try {
       $.get("indices/" + word, null, function(index) {
@@ -68,17 +72,17 @@ function tapIndex(word, searchString) {
           }
         }
         indices[word] = index;
-        tapComplete(word, searchString);
+        tapComplete(meta, word, searchString);
       }, "text");
     } catch (error) {
-      tapComplete(word, searchString);
+      tapComplete(meta, word, searchString);
     }
   } else {
-    tapComplete(word, searchString);
+    tapComplete(meta, word, searchString);
   }
 }
 
-function tapComplete(word, searchString) {
+function tapComplete(meta, word, searchString) {
   untappedIndices--;
   if (untappedIndices == 0) {
     find_real(searchString);
