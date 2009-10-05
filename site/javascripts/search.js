ad2309ad2c7af495423e272f6d15819d3078012b
Marc Bowes <marcbowes@gmail.com>
2009-10-05 13:04:52 +0200

    meta searching working

--- site/javascripts/search.js
+++ site/javascripts/search.js
@@ -13,11 +13,11 @@ $(function() {
 function get_terms(string) {
   var terms = string.split(/[\s-\/]/);
   for (i = 0; i < terms.length; i++) {
-    var term = terms[i].replace(/[^\d\w]/g, "");
+    var term = terms[i];
     var idx = term.indexOf(":");
     terms[i] = new Array(2);
     terms[i][0] = (idx == -1 ? "" : term.substring(0, idx));
-    terms[i][1] = stemmer(idx == -1 ? term : term.substring(idx + 1));
+    terms[i][1] = stemmer(idx == -1 ? term : term.substring(idx + 1).replace(/[^\d\w]/g, ""));
   }
   return terms;
 }
@@ -39,7 +39,7 @@ function find_real(string) {
   
   /* for some reason a foreach prints integers? */
   for (i = 0; i < terms.length; i++) {
-    var index = indices[terms[i][1]];
+    var index = indices[terms[i][0]][terms[i][1]];
     for (item in index) {
       if (!items[item]) {
         items[item] = index[item];
@@ -60,9 +60,9 @@ function find_real(string) {
 }
 
 function tapIndex(meta, word, searchString) {
-  if (!indices[word]) {
+  if (!indices[meta] || !indices[meta][word]) {
     try {
-      $.get("indices/" + word, null, function(index) {
+      $.get("indices/" + (meta == "" ? "" : meta + "/") + word, null, function(index) {
         var listings = index.split("\n");
         var index = new Array();
         for (i = 0; i < listings.length; i++) {
@@ -71,7 +71,9 @@ function tapIndex(meta, word, searchString) {
             index[documentToCount[0]] = parseInt(documentToCount[1]);
           }
         }
-        indices[word] = index;
+        if (!indices[meta])
+          indices[meta] = new Array();
+        indices[meta][word] = index;
         tapComplete(meta, word, searchString);
       }, "text");
     } catch (error) {
